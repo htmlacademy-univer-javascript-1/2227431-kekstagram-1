7a283094b1fe859ee6b16081551f13a142092f74
dmitr <dmitreicat@gmail.com>
2022-11-12 02:41:17 +0300

    module2-task1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const getRandomNumber = (from, to) => Math.floor(Math.random() * (to - from + 1) ) + from;
+const canFitMaxLength = (string, maxLength) => string.length <= maxLength;
+
+
+console.log(getRandomNumber(3, 5));
+console.log(canFitMaxLength('hello', 5));
+console.log(canFitMaxLength('hello', 4));
